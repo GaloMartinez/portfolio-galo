2ebfb8a42f1fdaba61a7b2cbe633230e0c2cfe6c
GaloMartinez <galomartinez1@hotmail.com>
2024-05-21 16:53:42 -0300

    commit

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import web1 from './../img/web1.jpg';
-import web2 from './../img/web2.png';
+import web1 from './../img/proyecto-portafolio.png';
+import web2 from './../img/proyecto-destileria.png';
 import web3 from './../img/web3.jpeg';
 
 import WorkIcon from '@mui/icons-material/Work';
@@ -14,10 +14,14 @@ function Proyects() {
                 Proyectos</h1>
             <div id="proyects" className="projects-container">
                 <div className="project">
+                <a href="https://master--curious-trifle-912ac3.netlify.app/#home" target="_blank" rel="noopener noreferrer">
                     <img src={web1} alt="Proyecto 1" />
+                    </a>
                 </div>
                 <div className="project">
+                <a href="https://destileria-ferlat-test.web.app/" target="_blank" rel="noopener noreferrer">
                     <img src={web2} alt="Proyecto 2" />
+                    </a>
                 </div>
                 <div className="project">
                     <img src={web3} alt="Proyecto 3" />
