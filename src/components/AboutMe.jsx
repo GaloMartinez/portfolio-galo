5500bc27915edbae5edc60e3cb7ea0f65ba9dbce
GaloMartinez <galomartinez1@hotmail.com>
2024-05-24 13:43:34 -0300

    commit

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,7 +9,7 @@ function AboutMe() {
       <div className='container-aboutme  '>
         <div className='aboutme-row row justify-content-around '>
           <div className='aboutme-column1 col-xl-7 col-lg-12 col-md-12 col-sm-12 '>
-            <Image className="img-aboutme" src={foto} width={600} height={350} alt='header-img' />
+            <Image className="img-aboutme" src={foto}  alt='header-img' />
           </div>
           <div className='aboutme-column2 col-xl-5 col-lg-12 col-md-12 col-sm-12 d-flex flex-column  '>
             <div className='description'>
@@ -17,7 +17,7 @@ function AboutMe() {
                 <FeedIcon className='icon-description ' sx={{ color: 'lightseagreen', fontSize: { xs: 30, sm: 40 } }} />
                 Acerca de mi
               </h1>
-              <p className='parrafo-aboutme'>Soy un desarrollador web frontend que construye el front-end de sitios web y aplicaciones web que conducen al éxito del producto en general. Echa un vistazo a algunos de mis trabajos en la sección de Proyectos .</p>
+              <p className='parrafo-aboutme'>Soy un apasionado de la tecnología con un fuerte interés en el desarrollo web y en la creación de experiencias de usuario excepcionales. Me destaco por mi capacidad para trabajar en equipo y mantener un enfoque constante en el crecimiento profesional. Estoy siempre interesado en afrontar nuevos desafíos y mejorar mis habilidades.</p>
               <div className="d-flex justify-content-start ">
                 <a href="#contact" data-for="contact" className='items-aboutme '>
                   <button className='button-contact'>
